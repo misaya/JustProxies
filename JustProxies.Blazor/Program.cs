c583a125d79df4df723dad8f2a668cdeabf82f51
misaya <xfhmailbox@msn.cn>
2024-05-07 13:55:53 +0800

    update；

---
+++ JustProxies.Blazor/Program.cs
@@ -0,0 +1,54 @@
+using JustProxies.Blazor.Components;
+using JustProxies.Integration.Core;
+using JustProxies.Integration.SkyEye;
+using JustProxies.Proxy;
+using JustProxies.Proxy.Core;
+using JustProxies.Proxy.Core.Options;
+using JustProxies.Proxy.Setup;
+using JustProxies.Proxy.Setup.Core;
+using JustProxies.RuleEngine;
+using JustProxies.RuleEngine.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region 处理IOptions配置
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true);
+builder.Services.Configure<HttpProxyServerOptions>(
+    builder.Configuration.GetSection(HttpProxyServerOptions.OptionsName));
+builder.Services.Configure<SkyEyeOptions>(
+    builder.Configuration.GetSection(SkyEyeOptions.OptionsName));
+
+#endregion
+
+#region 处理依赖注入
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<ISkyEyeIntegration, SkyEyeIntegration>();
+builder.Services.AddSingleton<IHttpProxyTool, HttpProxyTool>();
+builder.Services.AddSingleton<IRuleManagement, RuleManagement>();
+builder.Services.AddSingleton<IHttpInterceptor, HttpInterceptor>();
+builder.Services.AddSingleton<IHttpProxyServer, HttpProxyServer>();
+
+#endregion
+
+builder.Services.AddHostedService<IHttpProxyServer>(p => p.GetRequiredService<IHttpProxyServer>());
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddBootstrapBlazor();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.Run();
---
+++ JustProxies.Integration/SkyEye/SkyEyeIntegration.cs
@@ -0,0 +1,57 @@
+﻿using JustProxies.Common;
+using JustProxies.Integration.Core;
+using JustProxies.Integration.SkyEye.Exts;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace JustProxies.Integration.SkyEye;
+
+public class SkyEyeIntegration(
+    IOptions<SkyEyeOptions> options,
+    ILogger<SkyEyeIntegration> logger,
+    IHttpClientFactory httpClientFactory) : ISkyEyeIntegration
+{
+    private readonly SkyEyeOptions _options = options.Value;
+
+    public Task<List<string>> QueryAppUkListAsync()
+    {
+        var result = this._options.AppList.Select(p => p.AppUk).ToList();
+        return Task.FromResult(result);
+    }
+
+    public async Task<List<SkyEyeContent>> QueryAsync(string appUk, SkyEyeQueries query)
+    {
+        try
+        {
+            var app = this._options.AppList.Find(p => p.AppUk == appUk)!;
+            if (app.IsNull())
+            {
+                throw new ArgumentOutOfRangeException(nameof(appUk), $"{appUk}未配置appId及token");
+            }
+
+            query.AddApp(app.AppId, app.Token);
+
+            var httpclient = httpClientFactory.CreateClient();
+            httpclient.BaseAddress = new Uri(this._options.BaseUrl);
+            var result = await
+                httpclient.PostAsync(Constants.QueryRealTimeLogUrl, query.GetHttpContent(), CancellationToken.None);
+            result.EnsureSuccessStatusCode();
+            var text = await result.Content.ReadAsStringAsync();
+
+            var resp = text.ToObject<SkyEyeRespRoot>(true);
+            if (resp!.Code == 200 && resp.Result is { Count: > 0, List.Count: > 0 })
+            {
+                return resp.Result.List!;
+            }
+            else
+            {
+                throw new Exception($"返回内容:{text}");
+            }
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "查询失败");
+            return null!;
+        }
+    }
+}
